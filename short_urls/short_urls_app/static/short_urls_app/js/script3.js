217b7f0feb0b1527771fcc18ccf079eb9a71e0d1
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-03-27 23:04:03 +0300

    Update script3.js

--- short_urls/short_urls_app/static/short_urls_app/js/script3.js
+++ short_urls/short_urls_app/static/short_urls_app/js/script3.js
@@ -1,29 +1,44 @@
-window.onload = function() {
-  var dropdown = document.getElementById("template_list").onchange();
+window.onload = function () {
+    var dropdown = document.getElementById("template_list").onchange();
 };
 
 function populateDropdowns() {
-  var dropdown = document.getElementById('template_list');
-  if (dropdown.value === 'Site') {
-    Site_fillTemplate_TargetURL();
-  }
+    var dropdown = document.getElementById('template_list');
+    if (dropdown.value === 'Site') {
+        Site_template_target_url();
+        Site_template_utm_source();
+    }
 }
 
-function Site_fillTemplate_TargetURL() {
-  var data = [
-    {
-      "value": "https://givinschool.org/pto",
-      "text": "https://givinschool.org/pto"
+function fillSelect(dropdown, data) {
+    // Clear the dropdown list
+    dropdown.innerHTML = "";
+    // Add options to the dropdown list
+    for (var i = 0; i < data.length; i++) {
+        var option = document.createElement("option");
+        option.value = data[i].value;
+        option.text = data[i].text;
+        dropdown.add(option);
     }
-  ];
-  // Clear the dropdown list
-  var dropdown = document.getElementById('template_target_url');
-  dropdown.innerHTML = "";
-  // Add options to the dropdown list
-  for (var i = 0; i < data.length; i++) {
-    var option = document.createElement("option");
-    option.value = data[i].value;
-    option.text = data[i].text;
-    dropdown.add(option);
-  }
+}
+
+var mainTargetURL = [{
+        "value": "https://givinschool.org/pto",
+        "text": "https://givinschool.org/pto"
+    }
+];
+
+function Site_template_target_url() {
+    var dropdown = document.getElementById('template_target_url');
+    fillSelect(dropdown, mainTargetURL)
+}
+
+function Site_template_utm_source() {
+    var data = [{
+            "value": "givinschool_org",
+            "text": "givinschool_org"
+        }
+    ];
+    var dropdown = document.getElementById('template_utm_source');
+    fillSelect(dropdown, data)
 }
