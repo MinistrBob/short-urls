adfda005e1a5840ab26e1b83a1221a9c6447461a
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-03-28 09:32:44 +0300

    save

--- short_urls/short_urls_app/static/short_urls_app/js/script3.js
+++ short_urls/short_urls_app/static/short_urls_app/js/script3.js
@@ -7,6 +7,10 @@ function populateDropdowns() {
     if (dropdown.value === 'Site') {
         Site_template_target_url();
         Site_template_utm_source();
+        Site_template_utm_medium();
+        Site_template_utm_content();
+        Site_template_utm_campaign();
+        Site_template_utm_term();
     }
 }
 
@@ -18,18 +22,27 @@ function fillSelect(dropdown, data) {
         var option = document.createElement("option");
         option.value = data[i].value;
         option.text = data[i].text;
-        dropdown.add(option);
+//        dropdown.add(option);
+        dropdown.appendChild(option);
+        console.log("Added option %o for dropdown %o", option, dropdown);
     }
+    console.log("option[0]=%o", dropdown.options[0]);
+    dropdown.options[0].defaultSelected = true;
 }
 
 var mainTargetURL = [{
         "value": "https://givinschool.org/pto",
         "text": "https://givinschool.org/pto"
+    },
+    {
+        "value": "https://givinschool3.org/pto",
+        "text": "https://givinschool3.org/pto"
     }
 ];
 
 function Site_template_target_url() {
     var dropdown = document.getElementById('template_target_url');
+    console.log("dropdown=%o", dropdown);
     fillSelect(dropdown, mainTargetURL)
 }
 
@@ -40,5 +53,45 @@ function Site_template_utm_source() {
         }
     ];
     var dropdown = document.getElementById('template_utm_source');
+    fillSelect(dropdown, data)
+}
+
+function Site_template_utm_medium() {
+    var data = [{
+            "value": "event_page_GS",
+            "text": "event_page_GS"
+        }
+    ];
+    var dropdown = document.getElementById('template_utm_medium');
+    fillSelect(dropdown, data)
+}
+
+function Site_template_utm_content() {
+    var data = [{
+            "value": "post_anons",
+            "text": "post_anons"
+        }
+    ];
+    var dropdown = document.getElementById('template_utm_content');
+    fillSelect(dropdown, data)
+}
+
+function Site_template_utm_campaign() {
+    var data = [{
+            "value": "",
+            "text": ""
+        }
+    ];
+    var dropdown = document.getElementById('template_utm_campaign');
+    fillSelect(dropdown, data)
+}
+
+function Site_template_utm_term() {
+    var data = [{
+            "value": "",
+            "text": ""
+        }
+    ];
+    var dropdown = document.getElementById('template_utm_term');
     fillSelect(dropdown, data)
 }
