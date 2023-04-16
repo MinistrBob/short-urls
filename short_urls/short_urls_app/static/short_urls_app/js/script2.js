5662dc19e08a04cdbcf6873d6423fb0bfbd2ea8a
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-04-16 11:48:27 +0300

    delete js

--- short_urls/short_urls_app/static/short_urls_app/js/script2.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $.getJSON('http://127.0.0.1:8000/static/short_urls_app/data/data.json', function(data) {
-    $.each(data, function(i, option) {
-      $('#sel').append($('<option/>').attr("value", option.id).text(option.name));
-    });
-  });
-});
