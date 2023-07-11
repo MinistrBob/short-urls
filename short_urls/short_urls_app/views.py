affde40af0becb6a61ffb60deda5a1f36b3d328c
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-07-11 23:21:46 +0300

    TODO extract

--- short_urls/short_urls_app/views.py
+++ short_urls/short_urls_app/views.py
@@ -1,7 +1,3 @@
-# TODO: В списке ссылок поле short_link нужно выводить короткую ссылку целиком, чтобы было удобно копировать. Или сделать кнопочку копировать, чтобы сразу в буфер обмена копировалось.
-# TODO: Не нужно кнопку, нужно просто форму создания возвращать сразу с проставленным слагом, если человек хочет, то может удалить его и поставить свой. Сделать кнопку генерации слага - слаг нужно проверять на уникальность.
-# TODO: Добавить в Link creater, editor (те кто создал ссылку и последний редактор). https://docs.djangoproject.com/en/4.1/topics/class-based-views/generic-editing/#models-and-request-user
-# TODO: При заходе на s.gs.org перебрасывать на основной сайт школы.   /app/home
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect, get_object_or_404
